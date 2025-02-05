c2d42ae3f916a0526fbd18e6af3b391926cd9f8e
farzona-hasanova <tohirovmamurjon99@gmail.com>
2025-02-05 11:02:29 +0500

    hsdajhv

---
+++ java.js
@@ -0,0 +1,7 @@
+    // Foydalanuvchidan ism va yosh so'rash
+    var name = prompt("Ismingizni kiriting:");
+    var age = prompt("Yoshingizni kiriting:");
+
+    // Ekranga xabar chiqarish
+    document.getElementById("message").textContent = "Salom, " + name + "! Sizning yoshingiz: " + age;
+
